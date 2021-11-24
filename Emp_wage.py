49f6cbbd87249f6aec0f835ce76f831e10739649
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 00:06:29 +0530

    [Zeehan]add(using switcher)

--- Emp_wage.py
+++ Emp_wage.py
@@ -1,29 +1,31 @@
 """
 @Author: Zeehan 
 @Date: 2021-24-11 13:00:30
-@Title : add part time employee and wage 
+@Title : emp wage using switcher 
 """
 """
 Description:
-     imported random library and declare 2 constant variable ans use if elif   
+     imported random library and declare 3 constant variable ans use if   
 Parameter:
-      
+      use emphur function with parameter i and switcher is a dict and we returned value with .get method
 Return:
-       randint is a function it return random integer
+       return key and value randint is a function it return random integer
 """
 import random
 IS_PART_TIME = 1
 IS_FULL_TIME = 2
 EMP_RATE_PER_HOUR = 20
-#empHrs = 0
-#empWage = 0
+empHrs = 0
+empWage = 0
 empCheck = random.randint(0, 3)
-if (empCheck == IS_PART_TIME):
-    empHrs = 4
-elif (empCheck == IS_FULL_TIME):
-    empHrs = 8
-else:
-    empHrs=0
-empWage = empHrs * EMP_RATE_PER_HOUR
+def empHrs(i):
+        switcher={
+                0:0,
+                1:4,
+                2:8,
+             }
+        return switcher.get(i,0)
+
+empWage = empHrs(empCheck) * EMP_RATE_PER_HOUR
 print(empWage)
  
