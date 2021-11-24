5800ccdc180fb0453088a099144b0eeb87fc16c3
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 00:02:03 +0530

    [Zeehan]add(add part time employee and wage)

--- Emp_wage.py
+++ Emp_wage.py
@@ -1,25 +1,29 @@
 """
 @Author: Zeehan 
 @Date: 2021-24-11 13:00:30
-@Title : calculate daily wage 
+@Title : add part time employee and wage 
 """
 """
 Description:
-     imported random library and declare 2 constant variable ans use if else   
+     imported random library and declare 2 constant variable ans use if elif   
 Parameter:
-        
+      
 Return:
        randint is a function it return random integer
 """
 import random
-IS_FULL_TIME = 1
-EMP_RATE_PER_HUR = 20
-#Emp_Hur=0
-#Emp_Wage=0
-empCheck = random.randint(0, 2)
-if empCheck == IS_FULL_TIME:
-    Emp_Hur=8
+IS_PART_TIME = 1
+IS_FULL_TIME = 2
+EMP_RATE_PER_HOUR = 20
+#empHrs = 0
+#empWage = 0
+empCheck = random.randint(0, 3)
+if (empCheck == IS_PART_TIME):
+    empHrs = 4
+elif (empCheck == IS_FULL_TIME):
+    empHrs = 8
 else:
-    Emp_Hur=0
-Emp_Wage= Emp_Hur * EMP_RATE_PER_HUR
-print( Emp_Wage)           
+    empHrs=0
+empWage = empHrs * EMP_RATE_PER_HOUR
+print(empWage)
+ 
