1c5c268f4ea57a3c4e70d0a765fd11f3742c87ed
Zeehan0133 <zyne0133@gmail.com>
2021-11-24 23:53:33 +0530

    [Zeehan]add(employee attendance)

--- Emp_wage.py
+++ Emp_wage.py
@@ -1,7 +1,20 @@
 """
 @Author: Zeehan 
 @Date: 2021-24-11 13:00:30
-@Title : welcome massage
+@Title : check emp[loyee attendance
 """
 """
- print("welcome to employee wage calculator") 
+Description:
+     Check whether a employee is present or not  
+Parameter:
+      
+Return:
+       randint is a function it return random integer
+"""
+import random
+FULL_TIME=1
+empCheck = random.randint(0, 2)
+if empCheck==FULL_TIME:
+    print("Employee is Present ")
+else:
+    print("Employee is Absent ")    
